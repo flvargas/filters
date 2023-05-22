5c25d7b56c2822aa2df48ea924673735426daeef
flvargas <vargas.flavio@gmail.com>
2023-05-22 14:51:26 -0300

    css

--- index.js
+++ index.js
@@ -18,25 +18,3 @@ document.querySelectorAll(btnFiltro).forEach(
     }
 );
 
-/*!SECTIONconst btnFiltro = ".menu-filters li a";
-
-document.querySelectorAll(btnFiltro).forEach(
-    button => {
-        button.addEventListener("click", (e) => { 
-            e.preventDefault();
-            document.querySelectorAll(btnFiltro).forEach(button => {button.classList.remove("active")}); // desativam todos os botões
-            button.classList.add("active"); // ativa o botão selecionado
-            let filter = button.dataset.filter;
-
-            if (filter != "todos") {
-                document.querySelectorAll('.card').forEach(card => {card.style.opacity = 0;});
-                document.querySelectorAll('.card[data-filter='+filter+']').forEach(card => {card.style.opacity = 1;});
-            } else {
-                document.querySelectorAll('.card').forEach(card => {card.style.opacity = 1;});
-            }
-        })    
-    }
-);
-
-*/
-
